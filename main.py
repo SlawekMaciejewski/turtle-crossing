f3444e2af2e919ae518605ff84527794ce47d987
SlawekMaciejewski <slawek74.m@interia.pl>
2021-01-26 07:56:39 +0100

    Added a player movement up.

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ screen = Screen()
 screen.title("The turtle crossing")
 screen.setup(width=600, height=600)
 screen.tracer(0)
+
 player = Player()
+
+screen.listen()
+screen.onkeypress(player.move, "Up")
 
 game_is_on = True
 while game_is_on:
--- player.py
+++ player.py
@@ -1,5 +1,9 @@
 from turtle import Turtle
 
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
 
 class Player(Turtle):
 
@@ -10,5 +14,8 @@ class Player(Turtle):
         self.start_position()
 
     def start_position(self):
-        self.goto(0, -280)
+        self.goto(STARTING_POSITION)
         self.setheading(90)
+
+    def move(self):
+        self.forward(MOVE_DISTANCE)
