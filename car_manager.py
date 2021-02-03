b17331a96ac62ae8edafec912d33e51da2e27986
SlawekMaciejewski <slawek74.m@interia.pl>
2021-02-03 08:33:34 +0100

    Generating cars in random colors and moving them from right to left.

--- car_manager.py
+++ car_manager.py
@@ -1,11 +1,33 @@
 from turtle import Turtle
+from random import randint
 
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class Car(Turtle):
+def random_color():
+    r = randint(0, 255)
+    g = randint(0, 255)
+    b = randint(0, 255)
+    return r, g, b
+
+
+class CarManager:
 
     def __init__(self):
-        super().__init__()
-        self.shape("square")
+        self.all_cars = []
+
+    def create_car(self):
+        random_chance = randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle(shape="square")
+            new_car.setheading(180)
+            new_car.color(random_color())
+            new_car.shapesize(stretch_len=2, stretch_wid=1)
+            new_car.penup()
+            new_car.goto(300, randint(-250, 250))
+            self.all_cars.append(new_car)
+
+    def move(self):
+        for car in self.all_cars:
+            car.forward(STARTING_MOVE_DISTANCE)
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import time
 from turtle import Screen
 from player import Player
-from car_manager import Car
+from car_manager import CarManager
 
 screen = Screen()
+screen.colormode(255)
 screen.title("The turtle crossing")
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
-car = Car()
+car_manager = CarManager()
 
 screen.listen()
 screen.onkeypress(player.move, "Up")
@@ -18,6 +19,8 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    car_manager.create_car()
+    car_manager.move()
     if player.ycor() == 280:
         player.start_position()
 
